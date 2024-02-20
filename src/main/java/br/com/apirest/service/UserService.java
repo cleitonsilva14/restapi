5503ba7e94ec833db99f5f352fd028f826b5477f
cleitonsilva14 <cleiton18ads@gmail.com>
2024-02-20 13:12:42 -0300

    Ajuste classe UserService.java

--- src/main/java/br/com/apirest/service/UserService.java
+++ src/main/java/br/com/apirest/service/UserService.java
@@ -1,5 +1,8 @@
 package br.com.apirest.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,29 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
 	
+	public User findById(Long id) {
+	 	Optional<User> optionalUser = userRepository.findById(id);
+	 	return optionalUser.orElse(null);
+	}
+	
+	public void delete(Long id) {
+		User user = new User();
+		user.setId(id);
+		userRepository.delete(user);
+	}
+	
+	public User update(Long id, User user) {
+		User userFound = findById(id);
+		if(userFound != null) {
+			return userRepository.save(user);
+		}else {
+			return user;
+		}
+	}
 	
 	public User save(User user) {
 		return userRepository.save(user);
